1fc3515db6ca5b0687c616e9944eabbd30c60dc5
Maria-M-Code <321510018@gov.il>
2020-08-21 10:37:24 +0300

    css addec

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Person.css'
 
 const person = (props) => {
 return (
-    <div>
+    <div className="UserOutput">
         <h1>I am {props.name} I am {props.age}</h1>
         <h1>{props.children}</h1>
     </div>
